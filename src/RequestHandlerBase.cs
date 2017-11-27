91832b259b9d4f7dd156f1fb31d56ca599d57f80
brian kim <brikim@microsoft.com>
2017-11-26 21:41:40 -0800

    read steam and parse form body for urlencoded, will merge this after I finish json parsing

--- src/RequestHandlerBase.cs
+++ src/RequestHandlerBase.cs
@@ -8,6 +8,7 @@ namespace Maple
 {
     public abstract class RequestHandlerBase : IRequestHandler
     {
+        private int bufferSize = 4096;
         protected HttpListenerContext Context { get; set; }
         protected Hashtable QueryString { get; set; }
         protected Hashtable Form { get; set; }
@@ -17,30 +18,70 @@ namespace Maple
 
             if (context.Request.RawUrl.Split('?').Length > 1)
             {
-                var queryString = context.Request.RawUrl.Split('?')[1];
-                var pairs = queryString.Split('&');
-                this.QueryString = new Hashtable(pairs.Length);
-                foreach (var pair in pairs)
+                var q = context.Request.RawUrl.Split('?')[1];
+                this.QueryString = ParseUrlPairs(q);
+            }
+
+            if (context.Request.ContentType == "application/x-www-form-urlencoded")
+            {
+                int i = 0;
+                int len = (int)this.Context.Request.ContentLength64;
+                byte[] buffer = new byte[bufferSize];
+                string result = string.Empty;
+
+                while (i * bufferSize <= len)
                 {
-                    var keyValue = pair.Split('=');
-                    this.QueryString.Add(keyValue[0], keyValue[1]);
+                    int min = Min(bufferSize, (len - (i * bufferSize)));
+                    this.Context.Request.InputStream.Read(buffer, 0, min);
+                    result += new String(Encoding.UTF8.GetChars(buffer, 0, min));
+                    i++;
                 }
 
-                // TODO: Read body and parse form for application/x-www-form-urlencoded
-
-                // TODO: Read body and parse JSON for application/json
+                this.Form = ParseUrlPairs(result);
             }
+
+            // TODO: Read body and parse JSON for application/json
         }
 
         protected void WriteToOutputStream(string data)
         {
             var payload = Encoding.UTF8.GetBytes(data);
+            this.Context.Response.ContentLength64 = data.Length;
+
             using (this.Context.Response.OutputStream)
             {
-                // TODO: buffer this
-                this.Context.Response.OutputStream.Write(payload, 0, payload.Length);
+                int i = 0;
+                byte[] buffer = new byte[bufferSize];
+
+                while (i* bufferSize <= data.Length)
+                {
+                    int min = Min(bufferSize, data.Length - (i * bufferSize));
+                    buffer = Encoding.UTF8.GetBytes(data.Substring(i * bufferSize, min));
+                    this.Context.Response.OutputStream.Write(buffer, 0, min);
+                    i++;
+                }
                 this.Context.Response.OutputStream.Flush();
             }
+        }
+
+        private Hashtable ParseUrlPairs(string s)
+        {
+            var pairs = s.Split('&');
+            Hashtable result = new Hashtable(pairs.Length);
+            foreach (var pair in pairs)
+            {
+                var keyValue = pair.Split('=');
+                result.Add(keyValue[0], keyValue[1]);
+            }
+            return result;
+        }
+
+        private int Min(int a, int b)
+        {
+            if (a < b)
+                return a;
+            else
+                return b;
         }
     }
 
