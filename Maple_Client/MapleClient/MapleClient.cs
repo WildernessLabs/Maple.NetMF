537d5cb673f8e2df18270e2dd011bc3389ab7045
Adrian Stevens <adrian.stevens@gmail.com>
2018-06-20 23:31:15 -0700

    updated Maple Client namespace to Maple

--- Maple_Client/MapleClient/MapleClient.cs
+++ Maple_Client/MapleClient/MapleClient.cs
@@ -7,7 +7,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Netduino.Foundation
+namespace Maple
 {
     public class MapleClient
     {
--- Maple_Client/MapleClient/ServerItem.cs
+++ Maple_Client/MapleClient/ServerItem.cs
@@ -1,4 +1,4 @@
-﻿namespace Netduino.Foundation
+﻿namespace Maple
 {
     public class ServerItem
     {
