3abd25094e871c3177eaf51108506fe53fabaf39
Brian Kim <lamebrain@gmail.com>
2018-06-20 21:49:50 -0700

    add basic udp listener sample app

---
+++ Maple_Sample_Listener/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maple_Sample_Listener
+{
+    class Program
+    {
+        const int MAPLE_SERVER_BROADCASTPORT = 17756;
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient(MAPLE_SERVER_BROADCASTPORT);
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            while (true)
+            {
+                Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                string returnData = Encoding.ASCII.GetString(receiveBytes);
+                Console.WriteLine(returnData);
+            }
+            
+        }
+    }
+}
